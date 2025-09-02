987957f36d4471010583a905985c443c3adb29d8
unknown <Meek Bello>
2025-09-02 15:07:43 -0400

    modified NTFS detection for windows 10 devices

--- scripts/windows_10_stig.py
+++ scripts/windows_10_stig.py
@@ -3,15 +3,37 @@ import platform, os, psutil
 
 def format_drive(drive_letter: str):
     # look up what psutil.disk_partitions does
-    for partition in psutil.disk_partitions(all=True):
+    for partition in psutil.disk_partitions(all=False):
         # look up what partion.mountpoint does
         if partition.mountpoint.upper() == drive_letter.upper():
             # look up what partition.fstype does
             if partition.fstype.upper() != "NTFS":
-                print(f"{drive_letter} is NOT a NTFS drive", end="| ")
-                return
+                print(f"{drive_letter} is NOT a NTFS drive, it is a {partition.fstype} drive.")
+                if partition.fstype.upper() != "CDFS" and  partition.fstype.upper() != "UDF":
+                    user_selection = input(f"Would you like to format {drive_letter}? (yes/no): ").lower()
+                    while user_selection != "yes" and user_selection != "no":
+                        print("Invalid input. Please try again.")
+                        user_selection = input(f"Would you like to format {drive_letter}? (yes/no): ").lower()
+                    
+                    if user_selection == "no":
+                        print(f"You chose not to format drive {drive_letter}. Be aware this introduces security vulnerabilities to your system.")
+                    else:
+                        print(f"Attempting to formatting {drive_letter} to NTFS...")
+
+                        format_command = f"format {drive_letter[0]}: /FS:NTFS /Q /Y"
+                        print(f"Command: {format_command}")
+
+                        return_code = os.system(format_command)
+                        if return_code == 0: 
+                            print(f"{drive_letter} NTFS formatted sucessfully.")
+                            return format_drive(drive_letter)
+                        else:
+                            print(f"Failed to format {drive_letter} to NTFS.")
+
+                return False
             
     print(f"{drive_letter} is a NTFS drive", end="| ")
+    return True
 
 def encrypt_drive(drive: str, pin: str):
     print(f"Beginning BitLocker encryption on {drive}", end="... ")
@@ -19,8 +41,8 @@ def encrypt_drive(drive: str, pin: str):
 
 def win_10_stigs():
     # look up what platform.platform() does
-    print(f"Beginning Windows 10 STIG on {platform.platform()}")
-    if platform.platform() == 'Windows-11-10.0.26100-SP0':
+    print(f"Beginning Windows 10 STIG on {platform.system()}.")
+    if platform.system() == 'Windows':
         print("Welcome to GTRI STIG Scripter.")
 
         pin = ""
@@ -33,9 +55,9 @@ def win_10_stigs():
 
         drives = os.listdrives()
         for i in range(1, len(drives) + 1):
-            format_drive(drives[i - 1])
-            encrypt_drive(drives[i - 1], pin)
-            print()
+            if (format_drive(drives[i - 1])):
+                encrypt_drive(drives[i - 1], pin)
+                print()
     else:
         print("ERROR: NOT WINDOWS: Please run this script on a Windows machine.")
 
